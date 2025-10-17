0632d36455963af934fca9b053bf09c6ac14d847
vsbron <vsbron@gmail.com>
2025-10-17 21:02:43 +0300

    Small favicons fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export const metadata: Metadata = {
     "NextBlogs is a modern multi-user blogging platform to read, create, and share articles with a clean, user-friendly experience.",
   icons: {
     icon: [
-      { url: "/favicon.ico", sizes: "any" },
+      { url: "/favicon.ico?v=2", sizes: "any" },
       { url: "/icon.svg", type: "image/svg+xml" },
       { url: "/apple-touch-icon.png", sizes: "180x180" },
     ],
