9e5b4ba41633e04eb04f056c6b911cc32f4e31be
vsbron <vsbron@gmail.com>
2025-10-18 18:01:24 +0300

    Minor logo fix

--- components/Footer/Copyrights.tsx
+++ components/Footer/Copyrights.tsx
@@ -2,7 +2,11 @@ function Copyrights() {
   // Returned JSX
   return (
     <div className="flex flex-col sm:flex-row items-start sm:items-center gap-x-4 text-sm">
-      <svg width="121" height="30" className="fill-foreground/90">
+      <svg
+        width="122"
+        height="30"
+        className="fill-foreground/90 w-[101px] h-[25px] sm:w-[122px] sm:h-[30px]"
+      >
         <title>NextBlog</title>
         <use href="/logo.svg#logo"></use>
       </svg>
