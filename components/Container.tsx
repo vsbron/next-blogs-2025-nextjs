d091deef94735dc69db5538705b355dd5217542d
vsbron <vsbron@gmail.com>
2025-10-20 11:18:28 +0300

    Wider content area

--- components/Container.tsx
+++ components/Container.tsx
@@ -10,7 +10,9 @@ type ContainerProps = {
 function Container({ className, children }: ContainerProps) {
   // Returned JSX
   return (
-    <div className={cn("max-w-7xl mx-auto px-4 sm:px-8", className)}>
+    <div
+      className={cn("max-w-[1400px] max-w- mx-auto px-4 sm:px-8", className)}
+    >
       {children}
     </div>
   );
