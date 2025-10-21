e0959881df8400a96c3b5de7de735201cb3f2247
vsbron <vsbron@gmail.com>
2025-10-21 11:53:57 +0300

    Improvement to the Index stats component

--- components/Index/IndexStats.tsx
+++ components/Index/IndexStats.tsx
@@ -4,124 +4,124 @@ import { EyeIcon, FilePenLine, ThumbsUpIcon } from "lucide-react";
 import SectionTitle from "../SectionTitle";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 
-import { limitPreview } from "@/utils/helpers";
-
 // Dummy data
 const viewsList = [
   {
     label: "Why Lions are Awesome? We have 101 reasons that prove it!",
-    href: "/",
+    href: "/1",
     views: 953,
+    likes: 0,
   },
   {
     label: "How to survive in a huge city",
-    href: "/",
+    href: "/2",
     views: 823,
   },
   {
     label: "AI solved another major problem",
-    href: "/",
+    href: "/3",
     views: 736,
   },
   {
     label: "Eat HEALTHY!",
-    href: "/",
+    href: "/4",
     views: 701,
   },
   {
     label: "UFOs were sighted again!",
-    href: "/",
+    href: "/5",
     views: 528,
   },
   {
     label: "Why Lions are Awesome? We have 102 reasons that prove it!",
-    href: "/",
+    href: "/6",
     views: 369,
   },
   {
     label: "How to survive in a big city",
-    href: "/",
+    href: "/7",
     views: 112,
   },
   {
     label: "AI solved another big problem",
-    href: "/",
+    href: "/8",
     views: 96,
   },
   {
     label: "Eat HEALTHY! Always",
-    href: "/",
+    href: "/9",
     views: 55,
   },
   {
     label: "UFOs were sighted again and again!",
-    href: "/",
+    href: "/10",
     views: 9,
   },
 ];
 const likesList = [
   {
     label: "Why Lions are Awesome? We have 101 reasons that prove it!",
-    href: "/",
+    href: "/1",
     likes: 953,
+    views: 0,
   },
   {
     label: "How to survive in a huge city",
-    href: "/",
+    href: "/2",
     likes: 823,
   },
   {
     label: "AI solved another major problem",
-    href: "/",
+    href: "/3",
     likes: 736,
   },
   {
     label: "Eat HEALTHY!",
-    href: "/",
+    href: "/4",
     likes: 701,
   },
   {
     label: "UFOs were sighted again!",
-    href: "/",
+    href: "/5",
     likes: 528,
   },
   {
     label: "Why Lions are Awesome? We have 102 reasons that prove it!",
-    href: "/",
+    href: "/6",
     likes: 369,
   },
   {
     label: "How to survive in a big city",
-    href: "/",
+    href: "/7",
     likes: 112,
   },
   {
     label: "AI solved another big problem",
-    href: "/",
+    href: "/8",
     likes: 96,
   },
   {
     label: "Eat HEALTHY! always",
-    href: "/",
+    href: "/9",
     likes: 55,
   },
   {
     label: "UFOs were sighted again and again!",
-    href: "/",
+    href: "/10",
     likes: 9,
   },
 ];
 const authorsList = [
-  { label: "ChatGPT", href: "/", posts: 45 },
-  { label: "Default user", href: "/", posts: 39 },
-  { label: "Stephen King", href: "/", posts: 32 },
-  { label: "VSBroN", href: "/", posts: 29 },
-  { label: "AnnonWriter", href: "/", posts: 25 },
-  { label: "Gurst", href: "/", posts: 24 },
-  { label: "GRRM", href: "/", posts: 15 },
-  { label: "The Blogger", href: "/", posts: 11 },
-  { label: "Unknown user", href: "/", posts: 8 },
-  { label: "User413594", href: "/", posts: 2 },
+  { label: "ChatGPT", href: "/1", posts: 45 },
+  { label: "Default user", href: "/2", posts: 39 },
+  { label: "Stephen King", href: "/3", posts: 32 },
+  { label: "VSBroN", href: "/4", posts: 29 },
+  { label: "AnnonWriter", href: "/5", posts: 25 },
+  { label: "Gurst", href: "/6", posts: 24 },
+  { label: "GRRM", href: "/7", posts: 15 },
+  { label: "The Blogger", href: "/8", posts: 11 },
+  { label: "Unknown user", href: "/9", posts: 8 },
+  { label: "User413594", href: "/10", posts: 2 },
 ];
 
 function IndexStats() {
@@ -130,10 +130,10 @@ function IndexStats() {
     <section className="pb-8">
       <SectionTitle as="h2">Top Posts & Users</SectionTitle>
       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-x-4 lg:gap-x-6 gap-y-6">
-        <StatsCard list={viewsList} title="Most liked posts" />
+        <StatsCard list={viewsList} title="Most viewed posts" />
         <StatsCard
           list={likesList}
-          title="Most viewed posts"
+          title="Most liked posts"
           className="md:hidden lg:block"
         />
         <StatsCard list={authorsList} title="User with most posts" />
@@ -157,32 +157,35 @@ type StatsCardProps = {
 
 // Helper component
 function StatsCard({ list, title, className }: StatsCardProps) {
+  // Classes for icons
+  const iconClass = "!w-5 !h-5 stroke-primary";
+  const iconClassSm = "!w-4 !h-4 stroke-primary";
+
   // Returned JSX
   return (
     <Card
       className={`gap-0 py-4 xs:py-6 px-6 truncate max-w-[450px] ${className}`}
     >
       <CardHeader className="text-xl font-poppins border-b-2 border-accent px-0 mb-3 flex items-center gap-x-2 pb-2">
-        {list[0].views && <EyeIcon className="!w-5 !h-5 stroke-primary" />}
-        {list[0].likes && <ThumbsUpIcon className="!w-5 !h-5 stroke-primary" />}
-        {list[0].posts && <FilePenLine className="!w-5 !h-5 stroke-primary" />}
+        {list[0].views ? <EyeIcon className={iconClass} /> : ""}
+        {list[0].likes ? <ThumbsUpIcon className={iconClass} /> : ""}
+        {list[0].posts ? <FilePenLine className={iconClass} /> : ""}
         {title}
       </CardHeader>
       <CardContent className="px-0">
         <ul className="!pl-0 flex flex-col gap-1 text-sm lg:text-base">
           {list.map(({ label, href, posts, views, likes }) => (
             <li
-              key={label}
+              key={href}
               className="flex justify-between items-center gap-x-4"
             >
               <Link href={href} className="link-primary single-line-preview">
                 {label}
               </Link>
-
               <div className="flex items-center gap-1 text-foreground/40 font-semibold text-sm min-w-11">
-                {views && <EyeIcon className="!w-4 !h-4 stroke-primary" />}
-                {likes && <ThumbsUpIcon className="!w-4 !h-4 stroke-primary" />}
-                {posts && <FilePenLine className="!w-4 !h-4 stroke-primary" />}
+                {views ? <EyeIcon className={iconClassSm} /> : ""}
+                {likes ? <ThumbsUpIcon className={iconClassSm} /> : ""}
+                {posts ? <FilePenLine className={iconClassSm} /> : ""}
                 {views || likes || posts}
               </div>
             </li>
