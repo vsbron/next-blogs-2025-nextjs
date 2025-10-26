9f4ce19d83c52eab0497ece029f2eb127353ec5a
vsbron <vsbron@gmail.com>
2025-10-26 15:21:43 +0300

    Add User details pending animations

--- app/error.tsx
+++ app/error.tsx
@@ -8,14 +8,17 @@ import CTASection from "@/components/CTASection";
 import IndexStats from "@/components/PostStats";
 import SectionTitle from "@/components/SectionTitle";
 
-export default function ErrorPage({
-  error,
-}: {
+// Props type
+type ErrorPageProps = {
   error: Error & { digest?: string };
-}) {
+};
+
+// The page
+export default function ErrorPage({ error }: ErrorPageProps) {
   // Get the router
   const router = useRouter();
 
+  // Log the error
   useEffect(() => {
     console.error(error);
   }, [error]);
@@ -27,9 +30,15 @@ export default function ErrorPage({
           <SectionTitle>Error</SectionTitle>
           <p>
             Oops, something went wrong on our end. Looks like the system hit a
-            snag while trying to load this page. Don&apos;t worry - we&apos;re
-            already looking into it. Try refreshing the page, or come back in a
-            bit once things are running smoothly again.
+            snag while trying to load this page.
+            <br />
+            <code className="bg-border px-2 py-1 inline-block my-1">
+              {error.name}
+            </code>
+            <br />
+            Don&apos;t worry - we&apos;re already looking into it. Try
+            refreshing the page, or come back in a bit once things are running
+            smoothly again.
           </p>
           <p>
             You can try going{" "}
--- components/Header/DropdownUserDetails.tsx
+++ components/Header/DropdownUserDetails.tsx
@@ -4,7 +4,8 @@ import { LogOut } from "lucide-react";
 import { useClerk } from "@clerk/nextjs";
 import { SignedIn } from "@clerk/clerk-react";
 
-import { Button } from "../ui/button";
+import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
 
 import defaultAvatar from "@/assets/defaultUser.png";
 import useUser from "@/hooks/useUser";
@@ -16,23 +17,37 @@ function DropdownUserDetails() {
 
   const { user, isPending } = useUser(clerkId);
 
+  // const isPending = true;
+
   // Returned JSX
   return (
     <div className="pt-2 pb-4 pl-8 pr-4 flex justify-between items-center gap-x-8">
       <div className="flex items-center gap-x-3">
-        <Image
-          src={user?.avatarUrl ?? defaultAvatar}
-          width={40}
-          height={40}
-          className="mt-0.5 rounded-full"
-          alt={user ? user.username : "Unknown user"}
-        />
-        <div className="text-sm">
-          <div>{user ? user.displayName : "Unknown user"}</div>
-          <div className="text-muted-foreground/75 text-sm">
-            {user ? user.username : "No username"}
-          </div>
-        </div>
+        {isPending ? (
+          <>
+            <Skeleton className="h-10 w-10 rounded-full mt-0.5" />
+            <div className="space-y-2">
+              <Skeleton className="h-3 w-15" />
+              <Skeleton className="h-3 w-12" />
+            </div>
+          </>
+        ) : (
+          <>
+            <Image
+              src={user?.avatarUrl ?? defaultAvatar}
+              width={40}
+              height={40}
+              className="mt-0.5 rounded-full"
+              alt={user ? user.username : "Unknown user"}
+            />
+            <div className="text-sm">
+              <div>{user ? user.displayName : "Unknown user"}</div>
+              <div className="text-muted-foreground/75 text-sm">
+                {user ? user.username : "No username"}
+              </div>
+            </div>
+          </>
+        )}
       </div>
       <SignedIn>
         <Button variant="outline" size="sm" onClick={() => signOut()}>
