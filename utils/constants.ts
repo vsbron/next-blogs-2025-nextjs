64108fa3ca8d534ea8833890b658da01b0ad87c9
vsbron <vsbron@gmail.com>
2025-11-10 13:52:30 +0300

    Add Categories for posts

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,3 +1,22 @@
 export const SITE_DOMAIN = "https://next-blogs-2025.vercel.app";
 export const MAX_IMAGE_FILE_SIZE = 3 * 1024 * 1024;
 export const BUCKET_NAME = "post-images";
+
+export const POST_CATEGORIES = [
+  "Art & Design",
+  "Business & Finance",
+  "Education & Learning",
+  "Food & Recipes",
+  "Gaming",
+  "Health & Fitness",
+  "Lifestyle",
+  "Movies & TV",
+  "Music",
+  "Personal Stories",
+  "Pro Wrestling",
+  "Programming & Development",
+  "Science & Nature",
+  "Sports",
+  "Technology",
+  "Travel",
+] as const;
