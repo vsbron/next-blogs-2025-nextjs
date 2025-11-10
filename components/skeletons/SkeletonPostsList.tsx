2853a50b24271bf24fa3def6d996f099bfda90c3
vsbron <vsbron@gmail.com>
2025-11-11 02:18:23 +0300

    Skeletons update

--- components/skeletons/SkeletonPostsList.tsx
+++ components/skeletons/SkeletonPostsList.tsx
@@ -19,9 +19,10 @@ function PostPreviewLineSkeleton() {
   // Returned JSX
   return (
     <Card className="p-0 gap-0 grid grid-cols-1 md:grid-cols-[120px_1fr]">
-      <Skeleton className="w-30 h-30 rounded-none hidden md:block" />
+      <Skeleton className="w-30 h-full rounded-none rounded-l-lg hidden md:block" />
       <div className="flex flex-col gap-y-2 p-4">
         <Skeleton className="max-w-60 h-3 md:h-4" />
+        <Skeleton className="max-w-40 h-3 md:h-4" />
         <Skeleton className="max-w-90 h-4 md:h-5" />
         <Skeleton className="max-w-150 h-3 md:h-4" />
       </div>
